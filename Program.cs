21f9e7286625d5ab434fdc18bb0cebcb66f8990a
Zizou <Zizou@Zizos-MacBook-Air.local>
2020-09-16 11:32:35 +0200

    skapat fil

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitting_sharper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
